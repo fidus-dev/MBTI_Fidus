96a1270e91e7a9d7bb1c72ae36b177a60b4bd881
yeonggyulim <lyg940128@gmail.com>
2020-07-19 21:39:39 +0900

    [frontend] add footer component

--- mbti_fidus_frontend/src/pages/MainPage.tsx
+++ mbti_fidus_frontend/src/pages/MainPage.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
 
-interface MainPageProps {
-    
-}
+interface MainPageProps {}
 
-const MainPage: React.FC<MainPageProps> = ({  }) => {
-    return (
-        <Header />
-    );
-}
+const MainPage: React.FC<MainPageProps> = ({}) => {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+};
 
 export default MainPage;

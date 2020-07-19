0ab17368dfbfa91b8cdd2e0f36c22b7e978a53e5
yeonggyulim <lyg940128@gmail.com>
2020-07-19 21:20:27 +0900

    [frontend] Add MainPage component

---
+++ mbti_fidus_frontend/src/pages/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/common/Header';
+
+interface MainPageProps {
+    
+}
+
+const MainPage: React.FC<MainPageProps> = ({  }) => {
+    return (
+        <Header />
+    );
+}
+
+export default MainPage;

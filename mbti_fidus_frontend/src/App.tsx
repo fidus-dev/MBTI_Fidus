f0f98e5d9a7ca1b4b108c517a54e281783303dab
HoseungShin <nwdt3309@gmail.com>
2020-08-03 23:30:00 +0900

    [feat] entry page 제작중

--- mbti_fidus_frontend/src/App.tsx
+++ mbti_fidus_frontend/src/App.tsx
@@ -2,12 +2,12 @@ import React, { FC } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import CarouselTest from './components/CarouselTest';
 import SurveyContainer from './containers/survey';
-
+import EntryPage from './components/pages/Anonymous/EntryPage';
 const App: FC = () => {
 	
 	return (
 		<Switch>
-			<Route exact path={'/'} component={SurveyContainer} />
+			<Route exact path={'/'} component={EntryPage} />
 		</Switch>
 	)
 };
---
+++ mbti_fidus_frontend/src/components/pages/Anonymous/EntryPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface EntryPageProps {
+    
+}
+
+const EntryPage: React.FC<EntryPageProps> = ({  }) => {
+    return (
+        
+    );
+}
+
+export default EntryPage;

06aefbfdd8ad0d19a49d712a901f64ce091bb40f
HoseungShin <nwdt3309@gmail.com>
2020-08-12 13:31:39 +0900

    [refactor] 인터페이스 수정

--- mbti_fidus_frontend/src/components/Anonymous/Comment/CommentItem.tsx
+++ mbti_fidus_frontend/src/components/Anonymous/Comment/CommentItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled, { css } from 'styled-components';
-import { CommentItem } from './interfaces';
+import { IcommentItem } from './interfaces';
 import { MdSubdirectoryArrowRight, MdNotificationsNone } from 'react-icons/md';
 import getConfig from './CommentConfig';
 
@@ -33,10 +33,10 @@ interface StyleProps {
 interface CommentItemProps {
     width?: string;
     height?: string;
-    justifyContent: string;
-    fontWeight: string;
-    fontSize: string;
-    comment: CommentItem;
+    justifyContent?: string;
+    fontWeight?: string;
+    fontSize?: string;
+    comment: IcommentItem;
 }
 
 const CommentItem: React.FC<CommentItemProps> = ({ width, height, comment }) => {
@@ -47,7 +47,7 @@ const CommentItem: React.FC<CommentItemProps> = ({ width, height, comment }) => 
             {comment.depth !== 0 && <MdSubdirectoryArrowRight style={{ ...iconStyle, padding: 20 * comment.depth + 'px' }} />}
             <ItemDiv>{comment.id}</ItemDiv>
             <ItemDiv>{comment.content}</ItemDiv>
-            { whistleBlow && <MdNotificationsNone style={{...iconStyle}}/>}
+            {whistleBlow && <MdNotificationsNone style={{ ...iconStyle }} />}
         </WrapperDiv>
     );
 }
